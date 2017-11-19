9aff90bd49c37122a979a7e9fba1a52257f08b1c
sunzhengjie <sunzhengjie@kashuo.com>
2017-11-20 00:08:39 +0800

    还是代码生成吧

--- ui/src/view/esview/render.vue
+++ ui/src/view/esview/render.vue
@@ -1,84 +1,42 @@
 <template>
-  <Div>
-    <Form labelWidth="100">
-      <Row type="flex">
-        <Col span="10">
-        <FormItem label="input">
-          <Input v-model="modelInput">
-          </Input>
-        </FormItem>
-        <FormItem label="number">
-          <InputNumber v-model="modelInput" max="10" min="0">
-          </InputNumber>
-        </FormItem>
-        <FormItem label="date">
-          <DatePicker v-model="modelDatePicker" type="date" format="yyyy-MM-dd HH:mm:ss">
-          </DatePicker>
-        </FormItem>
-        <FormItem>
-          <Button type="primary">提交</Button>
-        </FormItem>
-        </Col>
-      </Row>
-    </Form>
-    <Row type="flex">
-      <Col span="24">
-      <Tabs>
-        <TabPane label="tab1">
-          <Table></Table>
-          <Row type="flex">
-            <Col span="10"> </Col>
-            <Col span="14">
-            <Page current="1" pageSize="10" total="100"></Page>
-            </Col>
-          </Row>
-        </TabPane>
-        <TabPane label="tab2">
-          <Table></Table>
-        </TabPane>
-      </Tabs>
-      </Col>
-    </Row>
-  </Div>
+  <div  class="render-container">
+    <h3 slot="title">{{pageName}}</h3>
+    <RenderDev style="    margin-top: 50px;" :soul="soul"></RenderDev>
+  </div>
+
 </template>
 <script>
-  export default {
-    data() {
-      return {
-        "modelInput": "",
-        "modelDatePicker": "",
-        "columns": [{
-          "title": "Name",
-          "key": "name"
-        }, {
-          "title": "Age",
-          "key": "age"
-        }, {
-          "title": "Address",
-          "key": "address"
-        }],
-        "data": [{
-          "name": "John Brown",
-          "age": 18,
-          "address": "New York No. 1 Lake Park",
-          "date": "2016-10-03"
-        }, {
-          "name": "Jim Green",
-          "age": 24,
-          "address": "London No. 1 Lake Park",
-          "date": "2016-10-01"
-        }, {
-          "name": "Joe Black",
-          "age": 30,
-          "address": "Sydney No. 1 Lake Park",
-          "date": "2016-10-02"
-        }, {
-          "name": "Jon Snow",
-          "age": 26,
-          "address": "Ottawa No. 2 Lake Park",
-          "date": "2016-10-04"
-        }]
-      }
+  import {
+    getRichPage
+  } from '../../resource/assemble_resource'
+  import{
+      parse
+  }from'../../util/assist'
+  import {mapGetters, mapMutations, mapActions} from 'vuex'
+  export default{
+    computed: {
+      ...mapGetters('dragModule', ['soul']),
+    },
+    methods:{
+      ...mapMutations('dragModule', ['setSoul']),
+    },
+    data(){
+        return {
+          pageName:''
+        }
+    },
+    mounted(){
+      getRichPage.call(this, this.$route.query.pageSoulId, (data) => {
+        this.pageName = data.name
+        let ancestorSoul = parse(data.pageSoul)
+        this.setSoul(ancestorSoul)
+      })
     }
   }
 </script>
+<style lang="less" scoped>
+  .render-container{
+    padding: 20px;
+  }
+
+</style>
